6e029603602e039021f60501d10b6e3bce7f7a7d
Sam Burnett <sam.burnett@gatech.edu>
2010-01-14 22:43:18 -0500

    Created python packaging

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='Collage',
+      version='1.0',
+      description='A censorship-resistent message layer',
+      author='Sam Burnett',
+      url='http://www.gtnoise.net/collage',
+      packages=['collage'],
+      )
