cb79c69b1360bc1c769238b2d196621f936a3ed1
Sam Burnett <sburnett@cc.gatech.edu>
2010-01-23 01:31:37 -0500

    Added support for CGI server script

--- newsubdir/server/server.py
+++ newsubdir/server/server.py
@@ -1,24 +1,15 @@
 #!/usr/bin/env python
 
-from SimpleXMLRPCServer import SimpleXMLRPCServer
+from SimpleXMLRPCServer import SimpleXMLRPCServer, CGIXMLRPCRequestHandler
 import xmlrpclib
 from optparse import OptionParser
 import datetime
+import os
 
 import database
 
-def main():
-    usage = 'usage: %s [options] <database> <vectors_dir> <application_name>'
-    parser = OptionParser(usage=usage)
-    parser.set_defaults(host='127.0.0.1', port='8000')
-    parser.add_option('-H', '--host', dest='host', action='store', type='string', help='Server hostname')
-    parser.add_option('-p', '--port', dest='port', action='store', type='int', help='Server port')
-    (options, args) = parser.parse_args()
-
-    if len(args) != 3:
-        parser.error('Need to specify database, vectors directory and application name')
-
-    db = database.DonationDatabase(args[0], args[2], args[1])
+def prep_funcs(db_file, vectors_dir, application_name):
+    db = database.DonationDatabase(db_file, application_name, vectors_dir)
 
     def donate(data, application, attributes, expiration=86400):
         tdexpiration = datetime.timedelta(0, expiration)
@@ -31,10 +22,40 @@ def main():
         else:
             return xmlrpclib.Binary(data)
 
+    return (donate, retrieve)
+
+def handle_cgi():
+    db_file = '/tmp/donation.sqlite'
+    vectors_dir = '/tmp/vectors'
+    app_name = 'test'
+
+    (donate, retrieve) = prep_funcs(db_file, vectors_dir, app_name)
+
+    handler = CGIXMLRPCRequestHandler()
+    handler.register_function(donate)
+    handler.register_function(retrieve)
+    handler.handle_request()
+
+def main():
+    usage = 'usage: %s [options] <database> <vectors_dir> <application_name>'
+    parser = OptionParser(usage=usage)
+    parser.set_defaults(host='127.0.0.1', port='8000')
+    parser.add_option('-H', '--host', dest='host', action='store', type='string', help='Server hostname')
+    parser.add_option('-p', '--port', dest='port', action='store', type='int', help='Server port')
+    (options, args) = parser.parse_args()
+
+    if len(args) != 3:
+        parser.error('Need to specify database, vectors directory and application name')
+
+    (donate, retrieve) = prep_funcs(args[0], args[1], args[2])
+
     server = SimpleXMLRPCServer((options.host, options.port))
     server.register_function(donate, 'donate')
     server.register_function(retrieve, 'retrieve')
     server.serve_forever()
 
 if __name__ == '__main__':
-    main()
+    if 'SERVER_NAME' in os.environ:
+        handle_cgi()
+    else:
+        main()
