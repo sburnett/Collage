69cc11a5cccd2b042ba5c932716c42988807f990
Sam Burnett <sburnett@cc.gatech.edu>
2010-02-02 05:17:47 -0500

    Add flickr app that searchs and published with tag pairs

--- newsubdir/tasks.py
+++ newsubdir/tasks.py
@@ -40,6 +40,43 @@ class DirectFlickrTask(Task):
     def can_embed(self, id, data):
         return True
 
+class TagPairFlickrTask(Task):
+    def __init__(self, flickr, tags):
+        self._flickr = flickr
+        self._tags = tags
+
+    def upload_progress(self, progress, done):
+        if done:
+            print 'Done uploading'
+        else:
+            print '%d%% done' % progress
+
+    def send(self, id, vector):
+        photo = tempfile.NamedTemporaryFile(suffix='.jpg')
+        photo.write(vector.get_data())
+        photo.flush()
+        title = base64.b64encode(id)
+
+        self._flickr.upload(filename=photo.name, title=title,
+                            description='', tags=' '.join(self._tags),
+                            callback=self.upload_progress)
+
+    def receive(self, id):
+        key = base64.b64encode(id)
+        # By default, search top 100 photos
+        results = self._flickr.photos_search(extras='url_o', tags=','.join(self._tags), tag_mode='any')
+
+        for photo in results[0]:
+            if 'title' in photo.attrib and photo.attrib['title'] == key:
+                try:
+                    data = urllib.urlopen(photo.attrib['url_o']).read()
+                    yield OutguessVector(data)
+                except Exception as e:
+                    print e.message
+
+    def can_embed(self, id, data):
+        return True
+
 class DirectTwitterTask(Task):
     def __init__(self, twitter, username, VectorClass):
         self._twitter = twitter
