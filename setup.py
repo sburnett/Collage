17460dfb2caaec8220f9e8029098fb6844a0a20a
Sam Burnett <sburnett@fenway.localdomain>
2010-04-19 18:12:26 -0400

    Reorganize

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 
-from distuils.core import setup
+from distutils.core import setup
 
 setup(name='Collage',
       version='1.0',
       description='A censorship-resistent message layer',
       author='Sam Burnett',
       url='http://www.gtnoise.net/collage',
-      packages=['collage'],
+      packages=['collage-core', 'collage-applications', 'collage-donation'],
       )
