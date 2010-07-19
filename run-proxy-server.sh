4f0d067b1aa3022c23f697aaf91ec6d4512ed127
root <root@brunello.dia.uniroma3.it>
2010-07-19 22:10:06 +0000

    Cleaned up Lighttpd configuration a bit

--- run-proxy-server.sh
+++ run-proxy-server.sh
@@ -8,7 +8,7 @@ COLLAGE_HOME=/home/collage
 
 # This is the directory where the Collage source is located.
 # It will be added to this session's PYTHONPATH
-COLLAGE_ROOT=/home/sburnett/git/collage
+COLLAGE_ROOT=$HOME/git/collage
 
 #####################################################################
 
@@ -16,6 +16,8 @@ COLLAGE_ROOT=/home/sburnett/git/collage
 # the screen session as the Collage user.
 cmd="cd $COLLAGE_HOME;
 export COLLAGE_ROOT=$COLLAGE_ROOT
+export COLLAGE_HOME=$COLLAGE_HOME
+export COLLAGE_USER=$COLLAGE_USER
 PYTHONPATH=$COLLAGE_ROOT;
 screen -c $COLLAGE_ROOT/proxy-screenrc"
 sudo -u $COLLAGE_USER -s -- script -q -c \"bash -c \\\"$cmd\\\"\" /dev/null
